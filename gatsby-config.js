46c59c3d3d1f466831a0892cfb87b2615c5665a0
gngsn <rudtjs4540@gmail.com>
2021-08-14 23:49:22 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "portfolio",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        accessToken: "",
+        spaceId: "",
+      },
+    },
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};

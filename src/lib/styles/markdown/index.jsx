0a3f84c7b0c548b725b85ce2e045c719e4c99bd1
gngsn <rudtjs4540@gmail.com>
2023-04-06 22:22:50 +0900

    update: move markdown component

--- src/lib/styles/markdown/index.jsx
+++
@@ -1,8 +0,0 @@
-export { default as BaseElement } from './BaseElement';
-export { default as Paragraph } from './Paragraph';
-export { default as QuotationMarksTitle } from './QuotationMarksTitle';
-export { default as FlexBox } from './FlexBox';
-export { default as SkillList } from './SkillList';
-export { default as ScrollImgContainer } from './ScrollImgContainer';
-export { default as Gallery } from './Gallery';
-export { default as LinkIcon } from './LinkIcon';

f5985983c1dc85a075a5f93e3764a179273b7554
gngsn <rudtjs4540@gmail.com>
2023-05-16 23:27:40 +0900

    update: modify moto details

--- src/components/home/outter/Bubble.jsx
+++ src/components/home/outter/Bubble.jsx
@@ -10,9 +10,9 @@ const Bubble = () => {
     const [screen, setScreen] = useState(getScreenSize());
 
     const motoList = [
-        { id: 0, icon: "/svg/moto/growth.svg", iconWidth: '30%', title: "GROWTH.", desc: "I am a hard worker and a fast learner who is always eager to explore new knowledge and keep up with the latest technology trends.", descKr: "."},
-        { id: 1, icon: "/svg/moto/persistence.svg", iconWidth: '40%', title: "PERSISTENCE.", desc: "Constantly striving for success. I am not afraid to fail, as I view it as a stepping stone towards achieving my goals.", descKr: "." },
-        { id: 2, icon: "/svg/moto/challenge.svg", iconWidth: '30%', title: "CHALLENGE.", desc: "Embrace challenges as a means of expanding knowledge, growth and self-improvement. Failure gives the proper perspective on success.", descKr: "."}
+        { id: 0, icon: "/svg/moto/growth.svg", iconWidth: '20%', title: "GROWTH.", desc: "I am a hard worker and a fast learner who is always eager to explore new knowledge and keep up with the latest technology trends.", descKr: "."},
+        { id: 1, icon: "/svg/moto/persistence.svg", iconWidth: '25%', title: "PERSISTENCE.", desc: "Constantly striving for success. I am not afraid to fail, as I view it as a stepping stone towards achieving my goals.", descKr: "." },
+        { id: 2, icon: "/svg/moto/challenge.svg", iconWidth: '20%', title: "CHALLENGE.", desc: "Embrace challenges as a means of expanding knowledge, growth and self-improvement. Failure gives the proper perspective on success.", descKr: "."}
     ];
 
     useEffect(() => {
@@ -29,12 +29,12 @@ const Bubble = () => {
 
     return (
         <Block>
-            <h1>motto.</h1>
+            <h1>MOTTO.</h1>
             {
                 screen === 'small' ?
                 motoList.map(moto =>
                     <TextContainer key={moto.id} iconWidth={moto.iconWidth}>
-                        <img src={moto.icon} />
+                        <img src={moto.icon} alt={moto.title}/>
                         <h2> {moto.title} </h2>
                         <h3>
                             {moto.desc}
@@ -49,7 +49,7 @@ const Bubble = () => {
                             <Circle key={moto.id}>
                                 {/* TODO img style에 반영되도록 변경 */}
                                 <TextContainer iconWidth={moto.iconWidth}>  
-                                    <img src={moto.icon} />
+                                    <img src={moto.icon} alt={moto.title} />
                                     <h2> {moto.title} </h2>
                                     <h3>
                                         {moto.desc}
@@ -69,7 +69,7 @@ const Circle = styled.div`
     position: relative; 
     float: left; 
     color: ${palette.black0};
-    margin: auto -14px; 
+    margin: auto -19px; 
     border: 1.3px solid #414141a1;
     border-radius: 50%; 
     background-color: ${palette.ivory0};
@@ -82,6 +82,12 @@ const Circle = styled.div`
         0% {transform: scale(1.0) translate(0, 0px);} 
         100% {transform: scale(1.03) translate(0, -30px);}
     }
+
+    background: rgba(255, 255, 255, 0.24);
+    box-shadow: 0 4px 30px rgba(255, 255, 255, 0.1);
+    backdrop-filter: blur(5.9px);
+    -webkit-backdrop-filter: blur(5.9px);
+    border: .1px solid rgba(255, 255, 255, 0.3);
 
     &:nth-child(1) {height: 280px; width: 280px; margin-top: 140px; animation: bubble 6s ease-in-out alternate infinite; z-index: 1;}
     &:nth-child(2) {height: 480px; width: 480px; margin-top: 20px; animation: bubble 3s ease-in alternate infinite; z-index: 3;}
@@ -153,7 +159,7 @@ const Block = styled.div`
     background-color: ${palette.lavender1};
     h1 {
         font-size: 3.5rem;
-        margin-bottom: 100px;
+        margin-bottom: 70px;
         color: ${palette.black0}; 
         ${device.tablet} {
             font-size: 2.5rem;
